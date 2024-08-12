669b44c53856147db0bbbddd8f8b35c27ea0b292
Farzad Khan <farzadk10000@gmail.com>
2024-08-12 17:26:49 +0530

    add expiry time to backend

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 const mysql = require("mysql2");
 const express = require("express");
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 
 const app = express();
-app.use(cors({
-  origin: '*'
-}))
-app.use(express.json())
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
 const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
 
 db.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err.stack);
+    console.error("Error connecting to the database:", err.stack);
     return;
   }
-  console.log('Connected to the database as id', db.threadId);
-})
+  console.log("Connected to the database as id", db.threadId);
+});
 
-app.get('/', (req, res) => {
-  const q = "SELECT * FROM BannerInfo";
-  db.query(q, (err,data) => {
-    if(err) return res.json(err)
-    return res.json(data)
-  })
-})
+app.get("/", (req, res) => {
+  const q = "SELECT * FROM bannerData";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
-app.put('/', (req, res) => {
-  const values = [
-    req.body.description,
-    req.body.link,
-  ]
-  const q = "UPDATE BannerInfo SET `description` = ?, `link` = ? WHERE id = ? ";
-  db.query(q, [...values, 12],(err, data) => {
-    if(err) return res.json(err)
-    return res.json("Updated successfully 200")
-  })
-  
-})
+app.put("/", (req, res) => {
+  const values = [req.body.description, req.body.link, req.body.expiryTime];
+  const q =
+    "UPDATE bannerData SET `description` = ?, `link` = ?, `expiryTime` = ? WHERE id = ? ";
+  db.query(q, [...values, 12], (err, data) => {
+    if (err) return res.json(err);
+    return res.json("Updated successfully 200");
+  });
+});
 
 app.listen(8000, () => {
-  console.log('listening on port 8000')
-})
+  console.log("listening on port 8000");
+});
